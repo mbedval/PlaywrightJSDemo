7730d3de8d6622cdb225782c99947c86a1c8512c
JMBX <mbedval@gmail.com>
2024-11-15 22:48:26 +0530

    updated with few reported test
    There is new reporter used related to github action

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,14 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: [['html', { open: 'always' ,outputFolder: '.\\test-results'  }]],
+  // reporter: [['json', { open: 'always' ,PLAYWRIGHT_JSON_OUTPUT_NAME: 'results.json' }]],
+  // reporter: './my-awesome-reporter.ts',
+  reporter: [
+    ['list'],
+    ['@estruyf/github-actions-reporter']
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
